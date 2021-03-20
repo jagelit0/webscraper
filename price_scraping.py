711bd35d8e29e5e2c315cb9b2e5fcd337adeb7eb
jagelit0 <73878309+jagelit0@users.noreply.github.com>
2021-03-20 13:20:25 +0100

    Update price_scraping.py

--- price_scraping.py
+++ price_scraping.py
@@ -1,22 +1,24 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
 import requests
-import msvcrt
+#import msvcrt
 
 url = 'https://example.com' #URL
 page = requests.get(url)
 soup = BeautifulSoup(page.content ,"html.parser")
-default = int("109")
+default = "109"
 
-pre = soup.find('span', class_='overflow--wrap-off') #Scraping
-pre_text = pre.text                                   
-pre_act = int(pre_text.replace('€', ''))
+pre = soup.find('span', class_='overflow--wrap-off') #Scraping (CHANGE)
+pre_text = pre.text
+pre_act = pre_text.replace('€', '')
 
 if default < pre_act :
-    print("Ha bajado de precio -> " + '(' + str(pre_act) + '€' + ') [*]' )
+    print("Ha bajado de precio -> " + '(' + str(pre_act) + '€' + ') [*]' )      #CHANGE
 elif default > pre_act :
     print("Ha subido -> " + '(' + str(pre_act) + '€' + ') [*]' )
 else :
    print("[*] Sigue igual -> " + '(' + str(pre_act) + '€' + ') [*]' )
 
-print("[!] Presiona una tecla para salir [!]")
-msvcrt.getch()
+#print("[!] Presiona una tecla para salir [!]")
+#msvcrt.getch()
